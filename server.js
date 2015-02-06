c534306462347478cea85d4cc1f6eb58e518d534
hylli <otto.hylli@tut.fi>
2015-02-06 21:56:10 +0200

    Made the HTTP server into a module that is used from index.js.

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 ﻿var http = require( "http" );
 
-http.createServer( function( request, response ) {
-    response.writeHead( 200, { "content-type": "text/plain" } );
-    response.write( "Hello, world!" );
-    response.end();
- }).listen( 9000 );
+var port = 9000;
+function start()
+{
+    function onRequest( request, response ) 
+    {
+        response.writeHead( 200, { "content-type": "text/plain" } );
+        response.write( "Hello, world!" );
+        response.end();
+    }
+ 
+    var server = http.createServer( onRequest );
+    server.listen( port );
+    console.log( "The server has started. Listening on port " +port +"." );
+}
+ 
+exports.start = start;
