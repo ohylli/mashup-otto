7de3b6776e7b340222cea38ef392f7a7e1d6467b
hylli <otto.hylli@tut.fi>
2015-02-06 22:03:41 +0200

    The server now prints the request path to the console.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 ﻿var http = require( "http" );
+var url = require( "url" );
 
 var port = 9000;
 function start()
 {
     function onRequest( request, response ) 
     {
+        var path = url.parse( request.url ).pathname;
+        console.log( "request to " +path );
         response.writeHead( 200, { "content-type": "text/plain" } );
         response.write( "Hello, world!" );
         response.end();
