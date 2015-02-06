2a22c45cc46a51701b7a46151f201b2c8d1f01a1
hylli <otto.hylli@tut.fi>
2015-02-06 21:24:18 +0200

    Implemented a hello world HTTP server.

---
+++ server.js
@@ -0,0 +1,7 @@
+﻿var http = require( "http" );
+
+http.createServer( function( request, response ) {
+    response.writeHead( 200, { "content-type": "text/plain" } );
+    response.write( "Hello, world!" );
+    response.end();
+ }).listen( 9000 );
